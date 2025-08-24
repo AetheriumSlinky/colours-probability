686b157e919a34a7303887ba4881d81da74d67a5
AetheriumSlinky <horveli@hotmail.com>
2025-08-24 11:54:23 +0300

    delete an unnecessary wrapper func

--- func/query.py
+++ func/query.py
@@ -18,57 +18,6 @@ class Query:
         self.cards = None
 
     @staticmethod
-    def handle_input_exceptions(func):
-        """
-        Handles 'exit' and 'clear' ('skip') inputs. Throws errors if inputs are not suitable, i.e. not a single string.
-        :param func: A function that strictly takes a string argument with the input() function.
-        :return: Function unless input was 'exit', 'clear' or 'skip'.
-        """
-
-        def wrapper(*args, **kwargs):
-            """Wrapper."""
-            new_arg = ([arg for arg in args if arg is not None]
-                       + [kwarg for kwarg in kwargs.values() if kwarg is not None])
-
-            if len(new_arg) != 1:
-                raise ValueError(
-                    f" > Unexpected number of args or kwargs (was {len(new_arg)}, should be 1). "
-                    f"The handle_input_exceptions decorator is probably used incorrectly. "
-                    f"The input should be a single str. Exiting program."
-                )
-
-            if not isinstance(new_arg[0], str):
-                raise TypeError(
-                    f" > Invalid data type (was {type(new_arg[0]).__name__}, should be str). "
-                    f"The handle_input_exceptions decorator is probably used incorrectly. "
-                    f"The input should be a single str. Exiting program."
-                )
-            new_arg = ''.join(new_arg).lower()
-
-            while True:
-
-                if new_arg in ['clear', 'skip']:
-                    raise ex.SkipToBeginning("Clear command was given.")
-                elif new_arg == 'exit':
-                    raise ex.ExitProgram("Exit command was given.")
-                elif not new_arg:
-                    print("Empty input. Please try again or enter 'skip' or 'exit'.")
-                else:
-                    try:
-                        return func(new_arg)
-                    except ex.InvalidInput as e:
-                        print(e)
-
-                new_arg = input("Try again to do whatever you were supposed to do:\n").lower()
-
-        return wrapper
-
-    @staticmethod
-    @handle_input_exceptions
-    def query_input(prompt: str) -> str:
-        return input(prompt)
-
-    @staticmethod
     def hello():
         print("""
 Hello!
@@ -78,7 +27,6 @@ In the Moxfield deck view click:
     -> ⋮More
     -> Export
     -> Copy for MTGO""")
-
 
     def ask_commander(self):
         while not self.commander:
